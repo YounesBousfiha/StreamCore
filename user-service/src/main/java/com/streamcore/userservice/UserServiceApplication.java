c1ab2d9c4d0f8644f82d98323ffc5dec4c0ac7de
Younes Bousfiha <125454968+YounesBousfiha@users.noreply.github.com>
2026-03-06 11:29:53 +0000

    feat: Add validation and logging imports to UserServiceApplication

--- user-service/src/main/java/com/streamcore/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/streamcore/userservice/UserServiceApplication.java
@@ -1,9 +1,14 @@
 package com.streamcore.userservice;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 @EnableFeignClients
